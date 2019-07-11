57ac92ddeb58b109b3b1a9b0881fbbf96df5a82d
Eszter <ekov@protonmail.ch>
2019-07-11 07:53:24 +0100

    feat(now): add more text

--- src/pages/now.js
+++ src/pages/now.js
@@ -8,9 +8,17 @@ export default function Now({location}) {
     <Layout location={location} title="About me">
       <SEO title="Now: what Iʼm up to" />
       <h1>Now</h1>
-      <p>
-        Loving Gatsby.
+      <p className="timestamp">
+        Last updated: 10 July, 2019
       </p>
+      <article>
+        <p>
+          <strong>In life</strong> I rediscovered the joy of preparing and eating simple and delicious food.
+        </p>
+        <p>
+          <strong>At work</strong> Iʼve been busy with Gatsby and Graphql. Iʼm also <a href="https://careers.fidel.uk/jobs/242473-front-end-engineer">looking for a frontend dev</a> (London/Lisbon, full time) to help me build <a href="https://fidel.uk">Fidel</a>ʼs tools for developers.
+        </p>
+      </article>
     </Layout>
   );
 }
