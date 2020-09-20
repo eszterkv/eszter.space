ff79cf6f47dc8344ba3de31f0d7c5c7b5fe66200
eszter <ekov@protonmail.ch>
2020-09-20 19:42:20 +0100

    add littlefoot for prettier footnotes

--- .eleventy.js
+++ .eleventy.js
@@ -2,8 +2,7 @@ var hljs = require('highlight.js')
 
 module.exports = function(config) {
   config.addPassthroughCopy({ public: './' })
-  config.addPassthroughCopy('./css/highlight.css')
-  config.addPassthroughCopy('./css/styles.css')
+  config.addPassthroughCopy('./css/')
 
   const highlight = (str, lang) => {
     if (lang && hljs.getLanguage(lang)) {
@@ -22,10 +21,6 @@ module.exports = function(config) {
     highlight,
   }
   const markdownLib = markdownIt(options).use(require('markdown-it-footnote'))
-  markdownLib.renderer.rules.footnote_block_open = () => (
-    '<h4 class="mt-10">Footnotes</h4>\n' +
-    '<ol class="list-decimal text-sm pl-5">\n'
-  );
 
   config.setLibrary('md', markdownLib)
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,15 @@
+const { colors } = require('tailwindcss/defaultTheme')
+
 module.exports = {
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: {
+          ...colors.blue,
+          '800': '#02f',
+        },
+      },
+    },
   },
   variants: {},
   plugins: [],
