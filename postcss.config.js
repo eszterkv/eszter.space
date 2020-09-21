a5d0dd8657b412061c39326357642763d53ab779
eszter <eszter@fidel.uk>
2020-09-21 19:03:22 +0100

    fix css build

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,5 @@
-const purgecss = require('@fullhuman/postcss-purgecss')
-
 module.exports = {
   plugins: [
-    purgecss({
-      content: ['*/**/*.html']
-    }),
     require('cssnano'),
     require('postcss-import'),
     require('autoprefixer'),
