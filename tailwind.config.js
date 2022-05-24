888272ffe0f6c07f1945d3512c80145a4cace836
eszterkv <ekov@pm.me>
2022-05-24 15:38:59 +0200

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,4 @@
-const { colors } = require('tailwindcss/defaultTheme')
-
 module.exports = {
-  theme: {
-    extend: {
-      colors: {
-        blue: {
-          ...colors.blue,
-          '800': '#02f',
-        },
-      },
-      fontFamily: {
-        display: ['PT Serif', 'Oswald'],
-      },
-    },
-  },
   plugins: [],
   future: {
     removeDeprecatedGapUtilities: true,
